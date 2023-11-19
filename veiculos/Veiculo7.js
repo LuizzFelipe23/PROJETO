ec93cda89b4e18d4883ed76a7d796c0ab9a8af21
LuizzFelipe23 <Luizfelipepotencialize@gmail.com>
2023-11-19 15:22:31 -0300

    arquivo 10.4
    alteração css, meu carrinho

--- veiculos/Veiculo7.js
+++ veiculos/Veiculo7.js
@@ -1,36 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Configurando as estatísticas e o preço para o veículo atual
-    const currentVehicle = {
+  // Configurando as estatísticas e o preço para o veículo atual
+  const currentVehicle = {
       stats: {
-        speed: 84,
-        acceleration: 75,
-        brake: 89,
-        traction: 82,
+          speed: 84,
+          acceleration: 75,
+          brake: 89,
+          traction: 82,
       },
-      price: 10000,
-    };
-  
-    // Chamando a função para atualizar as estatísticas na inicialização
-    updateStats(currentVehicle.stats);
-  
-    // Adicionando um ouvinte de evento para o botão de encomenda
-    const orderButton = document.getElementById("botaoPedido");
-    orderButton.addEventListener("click", function () {
+      price: 100000,
+  };
+
+  // Chamando a função para atualizar as estatísticas na inicialização
+  updateStats(currentVehicle.stats);
+
+  // Adicionando um ouvinte de evento para o botão de encomenda
+  const orderButton = document.getElementById("botaoPedido");
+  orderButton.addEventListener("click", function () {
+      // Chamando a função para tratar o pedido
       handleOrder(currentVehicle.price);
-    });
+
+      // Oculta o botão e exibe a mensagem de parabéns pela compra
+      orderButton.style.display = "none";
+      const mensagemCompra = document.getElementById("mensagemCompra");
+      mensagemCompra.style.display = "block";
   });
-  
-  // Função para atualizar as estatísticas na interface
-  function updateStats(stats) {
-    for (const statName in stats) {
+});
+
+// Função para atualizar as estatísticas na interface
+function updateStats(stats) {
+  for (const statName in stats) {
       if (stats.hasOwnProperty(statName)) {
-        const bar = document.getElementById(`${statName}Bar`);
-        const label = document.getElementById(`${statName}Label`);
-  
-        const value = stats[statName];
-  
-        bar.style.width = `${value}%`;
-        label.textContent = `${value}%`;
+          const bar = document.getElementById(`${statName}Bar`);
+          const label = document.getElementById(`${statName}Label`);
+
+          const value = stats[statName];
+
+          bar.style.width = `${value}%`;
+          label.textContent = `${value}%`;
       }
-    }
   }
+}
+
+// Função para tratar o pedido
+function handleOrder(price) {
+  // Adicione aqui a lógica para processar o pedido com o preço fornecido
+  console.log(`Pedido recebido com sucesso! Preço: $${price}`);
+}
